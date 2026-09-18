62bbc49c54e9318e93b17f19120fc8fd68423506
Desire5864 <zhangsan241111@gmail.com>
2026-09-19 00:45:51 +0800

    BdmvToIso v1.5.2: prefer full subtitle over cn_title

--- plugins.v2/bdmvtoiso/__init__.py
+++ plugins.v2/bdmvtoiso/__init__.py
@@ -50,7 +50,7 @@ class BdmvToIso(_PluginBase):
     # 插件图标
     plugin_icon = "UHD.png"
     # 插件版本
-    plugin_version = "1.5.1"
+    plugin_version = "1.5.2"
     # 插件作者
     plugin_author = "Desire5864"
     # 作者主页
